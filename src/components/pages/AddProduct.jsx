786fff8ee7a038e7adc4d0fffd670f7ac4ed4cfb
Braionic <trustedward@gmail.com>
2024-05-04 23:37:41 +0100

    i added extra select component

--- src/components/pages/AddProduct.jsx
+++ src/components/pages/AddProduct.jsx
@@ -55,6 +55,17 @@ export default function AddProduct() {
               <option value="3">Three</option>
             </select>
           </div>
+          <div className="mb-3">
+            <label className="form-check-label">
+              Select Brand
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
           <div className="mb-3 form-check">
             <TextInput type="color" label="Color" />
           </div>
